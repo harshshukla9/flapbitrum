760e1d3bd1b724637ee3d4153c965875f1c69b7b
Harsh Shukla <harshshukla@Harshs-MacBook-Air.local>
2025-08-22 19:32:40 +0530

    accountass

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -17,11 +17,6 @@ export async function GET() {
       "description": "Flapbitrum is a fun, crypto-native twist on the classic flappy bird game — built on Arbitrum. Play, flap, and dodge the blocks while repping Layer 2. Compete with friends, share scores, and explore the Arbi-verse with every flap.",
       "primaryCategory": "games"
     },
-    "accountAssociation": {
-      "header": "eyJmaWQiOjExMDg1NzQsInR5cGUiOiJhdXRoIiwia2V5IjoiMHhkODUzOTVERWYzZDYzM0U3ODYyOTFiZjlERTU0ZDMyNGVlYkM3OTE3In0",
-      "payload": "eyJkb21haW4iOiJhcmItZmxhcHB5dmVyc2UudmVyY2VsLmFwcCJ9",
-      "signature": "om5/KZjDvzcH6PSJk4J3QKxgGZkl0xfbU5m40l0e/zt9uX7iD6fpaXfSJeWMID5Eg6HvJRksbTpHBeBZ6HVNsBs="
-    }
   };
 
   return NextResponse.json(farcasterConfig);
