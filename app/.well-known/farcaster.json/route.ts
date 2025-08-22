d5d80e9991c3811c0ff7e9af6d123171bde2dcdc
Harsh Shukla <harshshukla@Harshs-MacBook-Air.local>
2025-08-22 20:15:00 +0530

    farcaster.jsonupdated2

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -3,32 +3,24 @@ import { APP_URL } from "../../../lib/constants";
 
 export async function GET() {
   const farcasterConfig = {
-    accountAssociation: {
-      header: "",
-      payload: "",
-      signature: ""
-    },
     frame: {
-      version: "1",
-      name: "Flapbitrum",
-      iconUrl: `${APP_URL}/images/arb.png`,
-      homeUrl: `${APP_URL}`,
-      imageUrl: `${APP_URL}/images/arb.png`,
-      screenshotUrls: [],
-      tags: ["base", "farcaster", "miniapp", "game"],
-      primaryCategory: "games",
-      buttonTitle: "Play Flapbitrum",
-      splashImageUrl: `${APP_URL}/images/splash.png`,
-      splashBackgroundColor: "#14051a",
-      subtitle: "Flap your way through Arbitrum",
-      description: "Flapbitrum is a fun, crypto-native twist on the classic flappy bird game — built on Arbitrum. Play, flap, and dodge the blocks while repping Layer 2. Compete with friends, share scores, and explore the Arbi-verse with every flap.",
-      webhookUrl: `${APP_URL}/api/webhook`,
-      tagline: "Flap your way through Arbitrum",
-      ogTitle: "Flapbitrum",
-      ogDescription: "Flapbitrum is a fun, crypto-native twist on the classic flappy bird game — built on Arbitrum. Play, flap, and dodge the blocks while repping Layer 2. Compete with friends, share scores, and explore the Arbi-verse with every flap.",
-      ogImageUrl: `${APP_URL}/images/arb.png`,
-      heroImageUrl: `${APP_URL}/images/arb.png`,
+      "name": "Flapbitrum",
+      "version": "1",
+      "iconUrl": "https://flapbitrum.vercel.app/icon.png",
+      "homeUrl": "https://flapbitrum.vercel.app",
+      "imageUrl": "https://flapbitrum.vercel.app/image.png",
+      "splashImageUrl": "https://flapbitrum.vercel.app/splash.png",
+      "splashBackgroundColor": "#ffffff",
+      "webhookUrl": "https://flapbitrum.vercel.app/api/webhook",
+      "subtitle": "Flap your way through Arbitrum",
+      "description": "Flapbitrum is a fun, crypto-native twist on the classic flappy bird game — built on Arbitrum. Play, flap, and dodge the blocks while repping Layer 2. Compete with friends, share scores, and explore the Arbi-verse with every flap.\"",
+      "primaryCategory": "games"
     },
+    "accountAssociation": {
+      "header": "eyJmaWQiOjExMDg1NzQsInR5cGUiOiJhdXRoIiwia2V5IjoiMHhkODUzOTVERWYzZDYzM0U3ODYyOTFiZjlERTU0ZDMyNGVlYkM3OTE3In0",
+      "payload": "eyJkb21haW4iOiJmbGFwYml0cnVtLnZlcmNlbC5hcHAifQ",
+      "signature": "4gTYaBgccYPoqlC9YVyq+HUV0zDxTAiC1cnbAOBIKBMFR69Wj+Y4osR2fAIi9ffUveTo9d6m7a7ftvdZ+HvXABw="
+    }
   };
 
   return NextResponse.json(farcasterConfig);
