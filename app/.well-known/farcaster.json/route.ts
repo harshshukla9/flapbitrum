b9aa3603942fec29d85d3697043e734e466e14e8
Harsh Shukla <harshshukla@Harshs-MacBook-Air.local>
2025-08-22 20:01:52 +0530

    farcaster.jsonupdated

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -3,24 +3,32 @@ import { APP_URL } from "../../../lib/constants";
 
 export async function GET() {
   const farcasterConfig = {
-    // TODO: Add your own account association
-    frame: {
-      frame: {
-        version: "1",
-        name: "Flapbitrum",
-        iconUrl: `${APP_URL}/images/arb.png`,
-        homeUrl: `${APP_URL}`,
-        imageUrl: `${APP_URL}/images/arb.png`,
-        screenshotUrls: [],
-        tags: ["monad", "farcaster", "miniapp", "template"],
-        primaryCategory: "developer-tools",
-        buttonTitle: "Play Flapbitrum",
-        splashImageUrl: `${APP_URL}/images/arb.png`,
-        splashBackgroundColor: "#ffffff",
-        webhookUrl: `${APP_URL}/api/webhook`,
-      },
+    accountAssociation: {
+      header: "",
+      payload: "",
+      signature: ""
     },
-   
+    frame: {
+      version: "1",
+      name: "Flapbitrum",
+      iconUrl: `${APP_URL}/images/arb.png`,
+      homeUrl: `${APP_URL}`,
+      imageUrl: `${APP_URL}/images/arb.png`,
+      screenshotUrls: [],
+      tags: ["base", "farcaster", "miniapp", "game"],
+      primaryCategory: "games",
+      buttonTitle: "Play Flapbitrum",
+      splashImageUrl: `${APP_URL}/images/splash.png`,
+      splashBackgroundColor: "#14051a",
+      subtitle: "Flap your way through Arbitrum",
+      description: "Flapbitrum is a fun, crypto-native twist on the classic flappy bird game — built on Arbitrum. Play, flap, and dodge the blocks while repping Layer 2. Compete with friends, share scores, and explore the Arbi-verse with every flap.",
+      webhookUrl: `${APP_URL}/api/webhook`,
+      tagline: "Flap your way through Arbitrum",
+      ogTitle: "Flapbitrum",
+      ogDescription: "Flapbitrum is a fun, crypto-native twist on the classic flappy bird game — built on Arbitrum. Play, flap, and dodge the blocks while repping Layer 2. Compete with friends, share scores, and explore the Arbi-verse with every flap.",
+      ogImageUrl: `${APP_URL}/images/arb.png`,
+      heroImageUrl: `${APP_URL}/images/arb.png`,
+    },
   };
 
   return NextResponse.json(farcasterConfig);
