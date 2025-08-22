77589f9eb1e13b6c8e5ceffd34a51867ecb99f8f
Harsh Shukla <harshshukla@Harshs-MacBook-Air.local>
2025-08-22 19:56:45 +0530

    farcaster.json

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -5,23 +5,22 @@ export async function GET() {
   const farcasterConfig = {
     // TODO: Add your own account association
     frame: {
-      "name": "Flapbitrum",
-      "version": "1",
-      "iconUrl": "https://arb-flappyverse.vercel.app/icon.png",
-      "homeUrl": "https://arb-flappyverse.vercel.app",
-      "imageUrl": "https://arb-flappyverse.vercel.app/image.png",
-      "splashImageUrl": "https://arb-flappyverse.vercel.app/splash.png",
-      "splashBackgroundColor": "#ffffff",
-      "webhookUrl": "https://arb-flappyverse.vercel.app/api/webhook",
-      "subtitle": "Flap your way through Arbitrum",
-      "description": "Flapbitrum is a fun, crypto-native twist on the classic flappy bird game — built on Arbitrum. Play, flap, and dodge the blocks while repping Layer 2. Compete with friends, share scores, and explore the Arbi-verse with every flap.",
-      "primaryCategory": "games"
+      frame: {
+        version: "1",
+        name: "Flapbitrum",
+        iconUrl: `${APP_URL}/images/arb.png`,
+        homeUrl: `${APP_URL}`,
+        imageUrl: `${APP_URL}/images/arb.png`,
+        screenshotUrls: [],
+        tags: ["monad", "farcaster", "miniapp", "template"],
+        primaryCategory: "developer-tools",
+        buttonTitle: "Play Flapbitrum",
+        splashImageUrl: `${APP_URL}/images/arb.png`,
+        splashBackgroundColor: "#ffffff",
+        webhookUrl: `${APP_URL}/api/webhook`,
+      },
     },
-    "accountAssociation": {
-      "header": "eyJmaWQiOjExMDg1NzQsInR5cGUiOiJhdXRoIiwia2V5IjoiMHhkODUzOTVERWYzZDYzM0U3ODYyOTFiZjlERTU0ZDMyNGVlYkM3OTE3In0",
-      "payload": "eyJkb21haW4iOiJmbGFwYml0cnVtLnZlcmNlbC5hcHAifQ",
-      "signature": "4gTYaBgccYPoqlC9YVyq+HUV0zDxTAiC1cnbAOBIKBMFR69Wj+Y4osR2fAIi9ffUveTo9d6m7a7ftvdZ+HvXABw="
-    }
+   
   };
 
   return NextResponse.json(farcasterConfig);
